df75fb52d460d25a0b15aafd9f2f0f864ba58ac0
Shivaram <naradashishivaram25@gmail.com>
2025-04-29 21:30:26 +0530

    Added Exercise - 16 Solutions

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,5 @@
+import {validDeliveryOption} from './deliveryOptions.js';
+
 export let cart;
 
 loadFromStorage();
@@ -90,6 +92,14 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
       matchingItem = cartItem;
     }
   });
+
+  if (!matchingItem) {
+    return;
+  }  
+
+  if (!validDeliveryOption(deliveryOptionId)) {
+    return;
+  }
 
   matchingItem.deliveryOptionId = deliveryOptionId;
 
